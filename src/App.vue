89e650975619d33d447e2e60e5ece52864c2c88f
novellac <38117965+novellac@users.noreply.github.com>
2021-12-20 00:38:27 -0500

    feat: Control synopsis with vmodeled inputs

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,185 @@
 <template>
-  <BaseInput
-    label="Name"
-    :model-value="myInput"
-    @update:model-value="myInput = $event"
+  <BaseRadioGroup
+    legend="What's your current shirt colour?"
+    groupName="shirts"
+    :options="shirts"
+    @update:model-value="
+      inputOne = shirts.find((el) => el.value === $event).meaning
+    "
   />
-  <code>{{ myInput }}</code>
-  <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+
+  <BaseRadioGroup
+    legend="What beverage are you drinking?"
+    groupName="beverages"
+    :options="beverages"
+    @update:model-value="
+      inputTwo = beverages.find((el) => el.value === $event).meaning
+    "
+  />
+
+  <BaseRadioGroup
+    legend="First letter of the last person you texted"
+    groupName="textees"
+    :options="textees"
+    @update:model-value="
+      inputThree = textees.find((el) => el.value === $event).meaning
+    "
+  />
+
+  <BaseRadioGroup
+    legend="First letter of the last thing you ate"
+    groupName="entrees"
+    :options="entrees"
+    @update:model-value="
+      inputFour = entrees.find((el) => el.value === $event).meaning
+    "
+  />
+
+  <Synopsis
+    :one="inputOne"
+    :two="inputTwo"
+    :three="inputThree"
+    :four="inputFour"
+  />
 </template>
 
 <script>
-import BaseInput from "./components/BaseInput.vue";
-import HelloWorldVue from "./components/HelloWorld.vue";
+import BaseRadioGroup from "./components/BaseRadioGroup.vue";
+import Synopsis from "./components/Synopsis.vue";
+
+import { ref } from "vue";
+
 export default {
   name: "App",
   components: {
-    BaseInput: BaseInput,
-    HelloWorld: HelloWorldVue,
+    BaseRadioGroup,
+    Synopsis
   },
   data() {
     return {
-      myInput: "bloop",
+      myInput: "bloop"
     };
   },
+  setup() {
+    const inputOne = ref(undefined);
+    const shirts = [
+      {
+        value: "red",
+        label: "Red",
+        meaning: "An orphaned"
+      },
+      {
+        value: "orange",
+        label: "Orange",
+        meaning: "A fashionably late"
+      },
+      {
+        value: "yellow",
+        label: "Yellow",
+        meaning: "A manic pixie"
+      },
+      {
+        value: "green",
+        label: "Green",
+        meaning: "A soul-searching"
+      },
+      {
+        value: "blue",
+        label: "Blue",
+        meaning: "An unlucky-in-love"
+      }
+    ];
+
+    const inputTwo = ref(undefined);
+    const beverages = [
+      {
+        value: "coffee",
+        label: "Coffee",
+        meaning: "corporate lawyer"
+      },
+      {
+        value: "tea",
+        label: "Tea",
+        meaning: "librarian"
+      },
+      {
+        value: "hotchocolate",
+        label: "Hot chocolate",
+        meaning: "former beauty queen"
+      },
+      {
+        value: "theraflu",
+        label: "Theraflu",
+        meaning: "therapist"
+      },
+      {
+        value: "hotapplecider",
+        label: "Hot apple cider",
+        meaning: "paleontologist"
+      }
+    ];
+
+    const inputThree = ref(undefined);
+    const textees = [
+      {
+        value: "a",
+        label: "A",
+        meaning: "foreclose on a small country inn"
+      },
+      {
+        value: "b",
+        label: "B",
+        meaning: "celebrate her two friends finally opening an inn"
+      },
+      {
+        value: "c",
+        label: "C",
+        meaning: "settle her parents' estate"
+      },
+      {
+        value: "d",
+        label: "D",
+        meaning: "teach a winter class at the community college"
+      }
+    ];
+
+    const inputFour = ref(undefined);
+    const entrees = [
+      {
+        value: "a",
+        label: "A",
+        meaning: "the town drunk"
+      },
+      {
+        value: "b",
+        label: "B",
+        meaning: "the widowed town librarian"
+      },
+      {
+        value: "c",
+        label: "C",
+        meaning: "Santa Claus"
+      },
+      {
+        value: "d",
+        label: "D",
+        meaning: "her childhood crush"
+      }
+    ];
+
+    return {
+      shirts,
+      inputOne,
+
+      beverages,
+      inputTwo,
+
+      textees,
+      inputThree,
+
+      entrees,
+      inputFour
+    };
+  }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Synopsis.vue
@@ -0,0 +1,39 @@
+<template>
+  <p>
+    <span class="bold"> {{ one }}&nbsp;{{ two }}</span> finds herself stranded
+    in her hometown during a pandemic lockdown, after she came home to
+    <span class="bold">{{ three }}</span
+    >. Against her better judgement, she falls in love with
+    <span class="bold">{{ four }}</span
+    >. Together, they learn the true meaning of Christmas.
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    one: {
+      type: String,
+      default: `[1]`
+    },
+    two: {
+      type: String,
+      default: `[2]`
+    },
+    three: {
+      type: String,
+      default: `[3]`
+    },
+    four: {
+      type: String,
+      default: `[4]`
+    }
+  }
+};
+</script>
+
+<style scoped>
+.bold {
+  font-weight: bold;
+}
+</style>
