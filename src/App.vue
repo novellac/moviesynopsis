4ec8317e6cc83196677af0f2446a869659de5427
novellac <38117965+novellac@users.noreply.github.com>
2021-12-19 23:18:05 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <BaseInput
+    label="Name"
+    :model-value="myInput"
+    @update:model-value="myInput = $event"
+  />
+  <code>{{ myInput }}</code>
+  <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+</template>
+
+<script>
+import BaseInput from "./components/BaseInput.vue";
+import HelloWorldVue from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    BaseInput: BaseInput,
+    HelloWorld: HelloWorldVue,
+  },
+  data() {
+    return {
+      myInput: "bloop",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <label :for="`${label}-input`">
+    {{ label }}
+  </label>
+  <input
+    :id="label"
+    type="text"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  props: ["modelValue", "label"],
+  emits: ["update:modelValue"],
+};
+</script>
