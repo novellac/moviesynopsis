40086ebc7b094ab771b5e012de696f15f834767d
novellac <38117965+novellac@users.noreply.github.com>
2021-12-20 00:37:48 -0500

    Add BaseRadio components

---
+++ src/components/BaseRadioButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <label>
+    <input
+      type="radio"
+      :name="groupName"
+      :value="value"
+      @change="$emit('update:modelValue', $event.target.value)"
+    />
+    {{ label }}
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    groupName: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
---
+++ src/components/BaseRadioGroup.vue
@@ -0,0 +1,37 @@
+<template>
+  <fieldset>
+    <legend>{{ legend }}</legend>
+    <BaseRadioButton
+      v-for="option in options"
+      :key="option.value"
+      :label="option.label"
+      :groupName="groupName"
+      :value="option.value"
+      v-bind="$attrs"
+    />
+  </fieldset>
+</template>
+
+<script>
+import BaseRadioButton from "./BaseRadioButton.vue";
+
+export default {
+  components: {
+    BaseRadioButton
+  },
+  props: {
+    options: {
+      type: Array,
+      default: () => []
+    },
+    groupName: {
+      type: String,
+      required: true
+    },
+    legend: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
